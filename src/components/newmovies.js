bb3bbed112bf7315f2db5d4e3f27cb8922da882f
Ronak Patel <ronaktk@gmail.com>
2018-09-12 13:03:11 -0400

    fetch data from api

--- src/components/newmovies.js
+++ src/components/newmovies.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react';
 
 class NewMovies extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        const key = '75ba52de60388bc096aefda1e8e28e2f';
+
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
+        .then(response => {
+            if(response.status !== 200) {
+                console.log('Error: ' + response.status);
+                return;
+            }
+            response.json().then(data => {
+                const movies = data.results;
+                console.log(movies);
+                this.setState({movies: movies});
+            });
+        })
+    }
+    
     render() {
         return(
             <div>
