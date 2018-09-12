df25dcb7d014f22387a1da7bd26f790753bfc9d2
Ronak Patel <ronaktk@gmail.com>
2018-09-12 10:35:45 -0400

    added recent movies component

---
+++ src/components/newmovies.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class NewMovies extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Latest Movies</h2>
+                <h3>Movie Name</h3>
+                <p>Movie Description</p>
+            </div>
+        );
+    }
+}
+
+export default NewMovies;
